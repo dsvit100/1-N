64795c8d7935fdd3fb1c6de585c5afabe6d8978c
hyojin shin <dsvit100hjin@gmail.com>
2025-03-24 12:12:42 +0900

    comment modeling/migration

--- articles/models.py
+++ articles/models.py
@@ -8,5 +8,10 @@ class Article(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
-# class Comment(models.Model):
-#     pass
+class Comment(models.Model):
+    content = models.TextField()
+    # 두 클래스를 foreingkey 관계로 묶어줌
+    # on_delete는 필수인자 = 부모가 지워졌을 때, 를 의미
+    article = models.ForeignKey(Article, on_delete=models.CASCADE) # models.CASCADE는 부모 내용이 지워지면 컨텐츠도 지워주세요요
+    # 나보다 한단계 위에 있는 모델
+    # 모델을 새로 만들었으면 번역본 만드는 코드를 다시 쳐줘야 함
